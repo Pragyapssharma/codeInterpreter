991b23f20cde263ffb0b3cf5816190627238bef2
pragya sharma <pragyasharma1209@gmail.com>
2025-06-02 11:17:11 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,44 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.err.println("Logs from your program will appear here!");
 
-    if (args.length < 2) {
-      System.err.println("Usage: ./your_program.sh tokenize <filename>");
-      System.exit(1);
+        if (args.length < 2) {
+            System.err.println("Usage: ./your_program.sh tokenize <filename>");
+            System.exit(1);
+        }
+
+        String command = args[0];
+        String filename = args[1];
+
+        if (!command.equals("tokenize")) {
+            System.err.println("Unknown command: " + command);
+            System.exit(1);
+        }
+
+        String fileContents = "";
+        try {
+            fileContents = Files.readString(Path.of(filename));
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(1);
+        }
+
+        for (char c : fileContents.toCharArray()) {
+            switch (c) {
+                case '(':
+                    System.out.println("LEFT_PAREN ( null");
+                    break;
+                case ')':
+                    System.out.println("RIGHT_PAREN ) null");
+                    break;
+                default:
+                    // Ignore other characters for now
+                    break;
+            }
+        }
+
+        System.out.println("EOF  null"); // End of file indicator
     }
-
-    String command = args[0];
-    String filename = args[1];
-
-    if (!command.equals("tokenize")) {
-      System.err.println("Unknown command: " + command);
-      System.exit(1);
-    }
-
-    String fileContents = "";
-    try {
-      fileContents = Files.readString(Path.of(filename));
-    } catch (IOException e) {
-      System.err.println("Error reading file: " + e.getMessage());
-      System.exit(1);
-    }
-
-     if (fileContents.length() > 0) {
-       throw new RuntimeException("Scanner not implemented");
-     } else {
-       System.out.println("EOF  null"); // Placeholder, replace this line when implementing the scanner
-     }
-  }
 }
