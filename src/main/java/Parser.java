b4f05fba77b009d1aba42d8c1430a975f15e3d53
pragya sharma <pragyasharma1209@gmail.com>
2025-06-05 10:35:58 +0530

    equality operator code

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -75,8 +75,8 @@ class Parser {
             return "(! " + expr + ")";
         } else if (match(TokenType.NUMBER)) {
             String lexeme = previous().lexeme;
-            if (lexeme.contains(".")) {
-                lexeme = lexeme.replaceAll("0+$", "").replaceAll("\\.$", "");
+            if (!lexeme.contains(".")) {
+                lexeme += ".0";
             }
             return lexeme;
         } else if (match(TokenType.STRING)) {
